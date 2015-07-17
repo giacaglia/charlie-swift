5dd3007f14b724598929c53b6b8ae6fc98cc90be
jcaralis <jim@cannonball.io>
2015-07-17 09:15:07 -0400

    added iCloud

--- charlie/tutorialViewController.swift
+++ charlie/tutorialViewController.swift
@@ -21,7 +21,10 @@ class tutorialViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        println("MEM")
     }
+
 
 
 }
