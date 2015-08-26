9b95195559307da297d89d738bee34078bc641d5
Jim Caralis <jim@blade.net>
2015-08-26 17:09:53 -0400

    can change worth it in group view with swipe

---
+++ charlie/SBGestureTableView/SBGestureTableViewGroupCellSideView.swift
@@ -0,0 +1,9 @@
+//
+//  SBGestureTableViewGroupCellSideView.swift
+//  Charlie
+//
+//  Created by Jim Caralis on 8/26/15.
+//  Copyright (c) 2015 James Caralis. All rights reserved.
+//
+
+import Foundation
