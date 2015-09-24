59cc4e14272895aa4b54a615b41d04731f844a5e
Giuliano Pezzolo Giacaglia <raulzito234@gmail.com>
2015-09-24 14:46:14 -0400

    Cleanup welcomeviewcontroller

--- charlie/welcomeViewController.swift
+++ charlie/welcomeViewController.swift
@@ -100,27 +100,35 @@ class welcomeViewController: UIViewController, UIScrollViewDelegate {
         pageControl.currentPage = 0
         pageControl.numberOfPages = pageCount
         
-        for _ in 0..<pageCount {
-            pageViews.append(nil)
-        }
-        
         let pagesScrollViewSize = scrollView.frame.size
         scrollView.contentSize = CGSize(width: pagesScrollViewSize.width * CGFloat(pageImages.count),
             height: pagesScrollViewSize.height)
-        loadVisiblePages()
+        for _ in 0..<pageCount {
+            pageViews.append(nil)
+        }
+        loadAllPages()
     }
     
+    func loginButtonAction(sender:UIButton!) {
+        charlieAnalytics.track("Onboarding Tutorial Completed")
+        performSegueWithIdentifier("toRegistration", sender: self)
+    }
     
-    func loadPage(page: Int) {
-        if page < 0 || page >= pageImages.count {
-            // If it's outside the range of what you have to display, then do nothing
-            return
-        }
-        
-        // 1
-        if let _ = pageViews[page] {
-            // Do nothing. The view is already loaded.
-        } else {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        // Load the pages that are now on screen
+        let pageWidth = scrollView.frame.size.width
+        let page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
+        pageControl.currentPage = page
+    }
+    
+    func loadAllPages() {
+        for page in 0...3 {
+            if page < 0 || page >= pageImages.count {
+                // If it's outside the range of what you have to display, then do nothing
+                return
+            }
+            
+            // 1
             var frame = scrollView.bounds
             frame.origin.x = frame.size.width * CGFloat(page)
             frame.origin.y = 0.0
@@ -129,7 +137,6 @@ class welcomeViewController: UIViewController, UIScrollViewDelegate {
             let newPageView = UIView()
             newPageView.backgroundColor = colors[page]
             newPageView.frame = frame
-            
             
             if page == 0 {
                 //tutorial title
@@ -178,7 +185,6 @@ class welcomeViewController: UIViewController, UIScrollViewDelegate {
             imageView.clipsToBounds = true
             imageView.layer.borderColor = UIColor.whiteColor().CGColor
             imageView.layer.borderWidth = 10
-            
             newPageView.addSubview(imageView)
             
             //loginbutton
@@ -197,62 +203,7 @@ class welcomeViewController: UIViewController, UIScrollViewDelegate {
             scrollView.addSubview(newPageView)
             // 4
             pageViews[page] = newPageView
-        }
-        
-        
-    }
-    
-    
-    func loginButtonAction(sender:UIButton!) {
-        charlieAnalytics.track("Onboarding Tutorial Completed")
-        performSegueWithIdentifier("toRegistration", sender: self)
-    }
-    
-    func purgePage(page: Int) {
-        if page < 0 || page >= pageImages.count {
-            // If it's outside the range of what you have to display, then do nothing
-            return
-        }
-        
-        // Remove a page from the scroll view and reset the container array
-        if let pageView = pageViews[page] {
-            pageView.removeFromSuperview()
-            pageViews[page] = nil
-        }
-    }
-    
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        // Load the pages that are now on screen
-        loadVisiblePages()
-    }
-    
-    
-    func loadVisiblePages() {
-        // First, determine which page is currently visible
-        let pageWidth = scrollView.frame.size.width
-        let page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
-        
-        // Update the page control
-        pageControl.currentPage = page
-        
-        // Work out which pages you want to load
-        let firstPage = page - 1
-        let lastPage = page + 1
-        
-        // Purge anything before the first page
-        for var index = 0; index < firstPage; ++index {
-            purgePage(index)
-        }
-        
-        // Load pages in our range
-        for index in firstPage...lastPage {
-            loadPage(index)
-        }
-        
-        // Purge anything after the last page
-        for var index = lastPage+1; index < pageImages.count; ++index {
-            purgePage(index)
-        }
+        }       
     }
     
 }
