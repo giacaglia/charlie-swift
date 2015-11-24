81f7290bd956beb7c2d0275f439acf70fba9440c
Giuliano Pezzolo Giacaglia <raulzito234@gmail.com>
2015-11-23 23:51:38 -0500

    Add new view controller and change filter button

---
+++ charlie/SwipedTransactionsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SwipedTransactions.swift
+//  charlie
+//
+//  Created by Giuliano Giacaglia on 11/23/15.
+//  Copyright © 2015 James Caralis. All rights reserved.
+//
+
+import Foundation
+
+class SwipedTransactionsViewController : UIViewController {
+    
+}
