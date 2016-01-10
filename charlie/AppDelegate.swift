96d993be7b6f7c30afd2d9f0af81e02c131e6ad1
jcaralis <jim@cannonball.io>
2016-01-10 14:05:48 -0500

    another try at migration

--- charlie/AppDelegate.swift
+++ charlie/AppDelegate.swift
@@ -66,12 +66,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             // Set the block which will be called automatically when opening a Realm with
             // a schema version lower than the one set above
+           
+            
             migrationBlock: { migration, oldSchemaVersion in
+         
+            migration.enumerate(User.className()) { oldObject, newObject in
+                
                 // We haven’t migrated anything yet, so oldSchemaVersion == 0
                 if (oldSchemaVersion < 1) {
                     // Nothing to do!
                     // Realm will automatically detect new properties and removed properties
                     // And will update the schema on disk automatically
+                    newObject!["happy_flow"] = 0.0
+                }
+                
                 }
         })
         
@@ -80,7 +88,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Now that we've told Realm how to handle the schema change, opening the file
         // will automatically perform the migration
-        let realm = try! Realm()
+       // let realm = try! Realm()
         
         
         
