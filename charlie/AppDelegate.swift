3df8e8df32343fa6f47bb969aec4527392632e10
jcaralis <jim@cannonball.io>
2016-01-10 14:27:37 -0500

    shuffled some code in app delegate to get migration working

--- charlie/AppDelegate.swift
+++ charlie/AppDelegate.swift
@@ -47,6 +47,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Mixpanel.sharedInstanceWithToken("77a88d24eaf156359e9e0617338ed328") //prod
         
         Mixpanel.sharedInstance().identify(Mixpanel.sharedInstance().distinctId)
+        
+        
+        
+             
+        
+        let configRealm = Realm.Configuration(
+            // Set the new schema version. This must be greater than the previously used
+            // version (if you've never set a schema version before, the version is 0).
+            schemaVersion: 1,
+            
+            // Set the block which will be called automatically when opening a Realm with
+            // a schema version lower than the one set above
+            
+            
+            migrationBlock: { migration, oldSchemaVersion in
+                
+                
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if (oldSchemaVersion < 1) {
+                    
+                    migration.enumerate(User.className()) { oldObject, newObject in
+                        
+                        // Nothing to do!
+                        // Realm will automatically detect new properties and removed properties
+                        // And will update the schema on disk automatically
+                        newObject!["happy_flow"] = 0.0
+                    }
+                    
+                }
+        })
+        
+        // Tell Realm to use this new configuration object for the default Realm
+        Realm.Configuration.defaultConfiguration = configRealm
+        
+        // Now that we've told Realm how to handle the schema change, opening the file
+        // will automatically perform the migration
+        let realm = try! Realm()
+
         charlieAnalytics.track("App Launched")
         
         var config = SwiftLoader.Config()
@@ -57,43 +95,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         config.foregroundColor = UIColor.blackColor()
         config.foregroundAlpha = 0.5
         SwiftLoader.setConfig(config)
-        
-        
-        let configRealm = Realm.Configuration(
-            // Set the new schema version. This must be greater than the previously used
-            // version (if you've never set a schema version before, the version is 0).
-            schemaVersion: 1,
-            
-            // Set the block which will be called automatically when opening a Realm with
-            // a schema version lower than the one set above
-           
-            
-            migrationBlock: { migration, oldSchemaVersion in
-         
-            migration.enumerate(User.className()) { oldObject, newObject in
-                
-                // We haven’t migrated anything yet, so oldSchemaVersion == 0
-                if (oldSchemaVersion < 1) {
-                    // Nothing to do!
-                    // Realm will automatically detect new properties and removed properties
-                    // And will update the schema on disk automatically
-                    newObject!["happy_flow"] = 0.0
-                }
-                
-                }
-        })
-        
-        // Tell Realm to use this new configuration object for the default Realm
-        Realm.Configuration.defaultConfiguration = configRealm
-        
-        // Now that we've told Realm how to handle the schema change, opening the file
-        // will automatically perform the migration
-       // let realm = try! Realm()
-        
-        
-        
-        
-        
+
         
         
         
